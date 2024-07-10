a7c5217c05635e4c46bf73c27484dc14f953bfaf
Preravit Sroemsakunwat <52848526+Preravit2545@users.noreply.github.com>
2024-07-10 10:57:41 +0000

    form UserManagement

--- src/contents/UserManagement.tsx
+++ src/contents/UserManagement.tsx
@@ -1,15 +1,40 @@
 function UserManagement() {
     return (
         <div className="content-wrapper">
-            {/* Content Header (Page header) */}
+
             <div className="content-header">
                 <div className="container-fluid">
-                    <div className="row mb-2">
-                        <div className="col-sm-6">
-                            <h1 className="m-0">จัดการข้อมูลผู้ใช้</h1>
-                        </div>
-                    </div>
+                    <h1>จัดการข้อมูลผู้ใช้</h1>
                 </div>
+            </div>
+
+            <div className="information">
+                <form action="">
+                    <div className="container-fluid">
+                        <div className="mb-3">
+                            <label htmlFor="username" className="form-label">Username :</label>
+                            <input type="text" className="form-control" placeholder="Enter name" />
+                        </div>
+
+                        <div className="mb-3">
+                            <label htmlFor="username" className="form-label">Password :</label>
+                            <input type="text" className="form-control" placeholder="Enter name" />
+                        </div>
+
+                        <div className="mb-3">
+                            <label htmlFor="username" className="form-label">UserRole :</label>
+                            <select className="form-control">
+                                <option value="เจ้าหน้าที่">เจ้าหน้าที่</option>
+                                <option value="อาจารย์">อาจารย์</option>
+                            </select>
+                        </div>
+                        <button className="btn btn-success">เพิ่มผู้ใช้</button>
+                    </div>
+                </form>
+            </div>
+            <hr />
+            <div className="user">
+                <button className="btn btn-primary">แสดงผู้ใช้</button>
             </div>
         </div>
     );
