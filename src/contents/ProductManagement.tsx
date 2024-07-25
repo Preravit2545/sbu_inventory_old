d6a0d6ade2620106a6876b9fc40cdbc7b5f92a1a
Preravit2545 <Preravit2545@gmail.com>
2024-07-25 14:54:01 +0700

    form user and product

--- src/contents/ProductManagement.tsx
+++ src/contents/ProductManagement.tsx
@@ -1,19 +1,57 @@
 function ProductManagement() {
     return (
         <div className="content-wrapper">
-            {/* Content Header (Page header) */}
+
             <div className="content-header">
                 <div className="container-fluid">
-                    <div className="row mb-2">
-                        <div className="col-sm-6">
-                            <h1 className="m-0">จัดการข้อมูลทรัพย์สิน</h1>
-                        </div>
-                    </div>
+                    <h1>จัดการข้อมูลทรัพย์สิน</h1>
                 </div>
+            </div>
+
+            <div className="information">
+                <form action="">
+                    <div className="container-fluid">
+                        <div className="mb-3">
+                            <label htmlFor="username" className="form-label">ชื่อทรัพย์สิน :</label>
+                            <input type="text" className="form-control" placeholder="Product name" />
+                        </div>
+
+                        <div className="mb-3">
+                            <label htmlFor="qty" className="form-label">จำนวน :</label>
+                            <input type="text" className="form-control" placeholder="0" />
+                        </div>
+
+                        <div className="mb-3">
+                            <label htmlFor="userRole" className="form-label">ประเภททรัพย์สิน :</label>
+                            <select className="form-control">
+                                <option value="เจ้าหน้าที่"> </option>
+                                <option value="อาจารย์"></option>
+                            </select>
+                        </div>
+
+                        <div className="mb-3">
+                            <label htmlFor="Status" className="form-label">สถานะทรัพย์สิน :</label>
+                            <select className="form-control">
+                                <option value="มี">มี</option>
+                                <option value="หมด">หมด</option>
+                            </select>
+                        </div> 
+
+                        <div className="mb-3">
+                            <label htmlFor="profilePicture" className="form-label">รูปสินค้า : </label>
+                            <input type="file" className="form-control" />
+                        </div>
+
+                        <button className="btn btn-success">เพิ่มผู้ใช้</button>
+                    </div>
+                </form>
+            </div>
+            <hr />
+            <div className="user">
+                <button className="btn btn-primary">แสดงผู้ใช้</button>
             </div>
         </div>
     );
 }
-
 
 export default ProductManagement;
--- src/contents/UserManagement.tsx
+++ src/contents/UserManagement.tsx
@@ -13,21 +13,39 @@ function UserManagement() {
                     <div className="container-fluid">
                         <div className="mb-3">
                             <label htmlFor="username" className="form-label">Username :</label>
-                            <input type="text" className="form-control" placeholder="Enter name" />
+                            <input type="text" className="form-control" placeholder="Enter Username" />
                         </div>
 
                         <div className="mb-3">
-                            <label htmlFor="username" className="form-label">Password :</label>
-                            <input type="text" className="form-control" placeholder="Enter name" />
+                            <label htmlFor="password" className="form-label">Password :</label>
+                            <input type="text" className="form-control" placeholder="Enter Password" />
+                        </div>
+
+
+                        <div className="mb-3">
+                            <label htmlFor="realname" className="form-label">ชื่อจริง :</label>
+                            <input type="text" className="form-control" placeholder="Enter Name" />
                         </div>
 
                         <div className="mb-3">
-                            <label htmlFor="username" className="form-label">UserRole :</label>
+                            <label htmlFor="surname" className="form-label">นามสกุล :</label>
+                            <input type="text" className="form-control" placeholder="Enter Surname" />
+                        </div>
+
+                        <div className="mb-3">
+                            <label htmlFor="userRole" className="form-label">ตำแหน่ง :</label>
                             <select className="form-control">
+                                <option value="Select">--เลือกตำแหน่ง--</option>    
                                 <option value="เจ้าหน้าที่">เจ้าหน้าที่</option>
                                 <option value="อาจารย์">อาจารย์</option>
                             </select>
                         </div>
+
+                        <div className="mb-3">
+                            <label htmlFor="profilePicture" className="form-label">Profile Picture :</label>
+                            <input type="file" className="form-control" />
+                        </div>
+
                         <button className="btn btn-success">เพิ่มผู้ใช้</button>
                     </div>
                 </form>
@@ -39,6 +57,5 @@ function UserManagement() {
         </div>
     );
 }
-
 
 export default UserManagement;
